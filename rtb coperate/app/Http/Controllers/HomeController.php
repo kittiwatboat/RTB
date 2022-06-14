71257c7b4909758f94d109dcf88e2ccbc8430e84
Aoner14 <jaturongsp@gmail.com>
2022-06-14 11:19:31 +0700

    hot_18

--- rtb coperate/app/Http/Controllers/HomeController.php
+++ rtb coperate/app/Http/Controllers/HomeController.php
@@ -42,8 +42,8 @@ class HomeController extends Controller
             'solution'=>$solution,
         ]);
     }
-    public function solutionStep2(){
-        $solution=solution2Model::get();
+    public function solutionStep2($id){
+        $solution=solution2Model::where('solutiontype_id',$id)->get();
         return view('fontend.solutionStep2',[
             'solution'=>$solution,
         ]);
@@ -54,8 +54,8 @@ class HomeController extends Controller
             'solution'=>$solution,
         ]);
     }
-    public function solutionStep3(){
-        $solution=solution3Model::get();
+    public function solutionStep3($id){
+        $solution=solution3Model::where('solution2_id',$id)->get();
         return view('fontend.solutionStep3',[
             'solution'=>$solution,
         ]);
@@ -66,8 +66,8 @@ class HomeController extends Controller
             'solution'=>$solution,
         ]);
     }
-    public function solutionStep4(){
-        $solution=solution4Model::get();
+    public function solutionStep4($id){
+        $solution=solution4Model::where('solution3_id',$id)->get();
         return view('fontend.solutionStep4',[
             'solution'=>$solution,
         ]);
